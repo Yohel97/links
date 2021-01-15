8a8a70540e7b8f313068c5dae6510a27079bade5
Brayan97 <brayanmedina5@gmail.com>
2021-01-15 12:04:31 -0600

    cambio de sistema operativo

--- src/keys.js
+++ src/keys.js
@@ -8,3 +8,12 @@ module.exports= {
 
     }
 };
+
+/*
+
+    database : {
+        user: 'beba83451187b6',
+        host: 'us-cdbr-east-02.cleardb.com',
+        password: 'a026820c',
+        database: 'heroku_dfabbe041e50584' 
+*/
